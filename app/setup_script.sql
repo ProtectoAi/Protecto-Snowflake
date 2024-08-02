e4112a97a02f489124fa66880bbf1c452a550871
Vivek Srinivasan <mail2vivekss@gmail.com>
2024-08-02 13:11:17 +0530

    Fixed deploy scripts

--- app/setup_script.sql
+++ app/setup_script.sql
@@ -3,16 +3,21 @@ CREATE APPLICATION ROLE IF NOT EXISTS protecto_app_role;
 
 -- 2. Create a versioned schema to hold those UDFs/Stored Procedures
 CREATE OR ALTER VERSIONED SCHEMA protecto_core;
+
+-- 3. Grant usage on the schema to the application role
 GRANT USAGE ON SCHEMA protecto_core TO APPLICATION ROLE protecto_app_role;
 
+-- 4. Create a stage in the schema
 CREATE OR REPLACE STAGE protecto_core.PROTECTO_STAGE;
+
+-- 5. Grant read and write privileges on the stage to the application role
 GRANT READ ON STAGE protecto_core.PROTECTO_STAGE TO APPLICATION ROLE protecto_app_role;
 GRANT WRITE ON STAGE protecto_core.PROTECTO_STAGE TO APPLICATION ROLE protecto_app_role;
 
--- 3. Create a streamlit object 
+-- 6. Create a Streamlit object
 CREATE OR REPLACE STREAMLIT protecto_core.PROTECTO_VAULT
      FROM '/streamlit/'
      MAIN_FILE = 'streamlit.py';
 
--- 4. Grant appropriate privileges over these objects to your application roles. 
+-- 7. Grant usage on the Streamlit object to the application role
 GRANT USAGE ON STREAMLIT protecto_core.PROTECTO_VAULT TO APPLICATION ROLE protecto_app_role;
--- scripts/pre_deploy.sql
+++ scripts/pre_deploy.sql
@@ -37,3 +37,7 @@ ENABLED = true;
 
 
 
+
+
+
+
